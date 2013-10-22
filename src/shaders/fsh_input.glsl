eca59cc0a581bb19589ec6fdebf6ad866db55d8f
Zach Fleischman <zach_fleischman@live.com>
2013-10-21 21:50:56 -0600

    this commit might be derpy

---
+++ src/shaders/fsh_input.glsl
@@ -0,0 +1,37 @@
+#version 420
+
+/* --------------- INPUT VARIABLES -------------- */
+/* In a fragment shader, attributes sent out with */
+/* processed vertices in the vertex shader        */
+/* and interpolated on the rasterization stage    */
+/* ---------------------------------------------- */
+
+
+
+
+
+/* ----------- OUTPUT VARIABLES ----------------- */
+/* For `simple' rendering we do here, there is    */ 
+/* just one: RGB value for the fragment           */
+/* ---------------------------------------------- */
+
+out vec3 fragcolor;
+
+
+
+/* ---------------------------------------------- */
+/* ----------- MAIN FUNCTION -------------------- */
+/* goal: compute the color of the fragment        */
+/*  [put it into the only output variable]        */
+/* ---------------------------------------------- */
+
+void main()
+{
+
+  fragcolor = vec3(
+                   0.5,
+                   0.0,
+                   0.4
+                  );
+  
+}
---
+++ src/triangle.h
@@ -0,0 +1,7 @@
+class Triangle
+{
+	public:
+	int a, b, c;
+
+	//Triangle(int _a, int _b, int _c) {a = _a; b = _b; c = _c;}
+};
